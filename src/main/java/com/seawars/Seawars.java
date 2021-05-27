4683b3fb08a6e8fa154c347b6e1a713979b048f2
alexandermilanovich <amilanov@vols.utk.edu>
2021-05-27 19:51:04 -0400

    Initial Setup
    Signed-off-by: alexandermilanovich <amilanov@vols.utk.edu>

--- src/main/java/com/seawars/Seawars.java
+++ src/main/java/com/seawars/Seawars.java
@@ -1,4 +1,17 @@
 package com.seawars;
 
-public class Seawars {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Seawars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("Seawars has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getLogger().info("Seawars has been disabled!");
+    }
 }
