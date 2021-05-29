89f7fa0c608ee61c96dd31c8c17ec2fa04a646dd
alexandermilanovich <amilanov@vols.utk.edu>
2021-05-29 14:57:06 -0400

    Database
    Signed-off-by: alexandermilanovich <amilanov@vols.utk.edu>

---
+++ src/main/java/com/seawars/database/MySQL.java
@@ -0,0 +1,67 @@
+package com.seawars.database;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class MySQL {
+
+    public static String host = "";
+    public static String port = "3306";
+    public static String database = "Main";
+    public static String username = "******";
+    public static String password = "*****";
+    public static Connection con;
+
+    static ConsoleCommandSender console = Bukkit.getConsoleSender();
+
+    // connect
+    public static void connect() {
+        if (!isConnected()) {
+            try {
+                con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, username, password);
+                console.sendMessage("\247c[\2476SeaWars\247c] \247bMySQL-Enabled!");
+                //PreparedStatement ps = MySQL.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS Players (Name VARCHAR(100),UUID VARCHAR(100),Kills INT(100),PRIMARY KEY (Name))");
+                //ps.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // disconnect
+    public static void disconnect() {
+        if (isConnected()) {
+            try {
+                con.close();
+                console.sendMessage("\247c[\2476SeaWars\247c]\247bMySQL-Disconnected!!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // isConnected
+    public static boolean isConnected() {
+        return (con != null);
+    }
+
+    // getConnection
+    public static Connection getConnection() {
+        return con;
+    }
+
+    //Example of storing player data into database
+    public static void storePlayer(UUID uuid, String name, int pkills) throws SQLException {
+        //PreparedStatement ps = MySQL.getConnection().prepareStatement("INSERT IGNORE INTO Players (Name,UUID,Kills) VALUES (?,?,?)");
+        //ps.setString(1, name);
+        //ps.setString(2, String.valueOf(uuid));
+        //ps.setInt(3, pkills);
+        //ps.executeUpdate();
+    }
+}
