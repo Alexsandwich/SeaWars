9404e57a219359b92689541c2ee804bacb81c639
DrunkCactus1 <forpavels@gmail.com>
2021-06-04 22:24:19 -0400

    Removed Obi, Added Oak Logs and Iron Block Reward System

--- src/main/java/com/seawars/events/blockPlace.java
+++ src/main/java/com/seawars/events/blockPlace.java
@@ -104,26 +104,34 @@ public class blockPlace implements Listener {
             if (redblock.contains(loc2.getBlock())) {
                 if (teamSystem.red.hasPlayer(player)) {
                     player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.RED + "You can not damage your own boat"));
-                } else if (loc2.getBlock().getType() == Material.OBSIDIAN) {
+                } else if (loc2.getBlock().getType() == Material.IRON_BLOCK) {
                     redobbymap.put(String.valueOf(player), ir + 1);
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.RED + "You've hit obsidian " + ir + "/5"));
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.RED + "You've hit Iron Block " + ir + "/5"));
+                    manager.addCurrencyToPlayer(player, 20);
+                    player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 20$")));
 
-                } else if (loc2.getBlock().getType() == Material.OAK_PLANKS){
+                } else if (loc2.getBlock().getType() == Material.OAK_LOG){
                     redwoodmap.put(String.valueOf(player), irw +1);
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.RED + "You've hit wood " + irw + "/2"));
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.RED + "You've hit Oak Log " + irw + "/2"));
+                    manager.addCurrencyToPlayer(player, 10);
+                    player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 10$")));
                 }
                 if (ir == 5) {
-                    if(loc2.getBlock().getType() == Material.OBSIDIAN) {
+                    if(loc2.getBlock().getType() == Material.IRON_BLOCK) {
                         loc2.getBlock().setType(Material.AIR);
                         redblock.remove(loc2);
                         redobbymap.replace(String.valueOf(player), 1);
+                        manager.addCurrencyToPlayer(player, 50);
+                        player.sendMessage(Utils.chat((ChatColor.GOLD + "You received 50$")));
                     }
                 }
                 if (irw == 2) {
-                    if(loc2.getBlock().getType() == Material.OAK_PLANKS) {
+                    if(loc2.getBlock().getType() == Material.OAK_LOG) {
                         loc2.getBlock().setType(Material.AIR);
                         redblock.remove(loc2);
                         redwoodmap.replace(String.valueOf(player), 1);
+                        manager.addCurrencyToPlayer(player, 20);
+                        player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 20$")));
                     }
                 }
             }
@@ -133,18 +141,20 @@ public class blockPlace implements Listener {
             if (blueblock.contains(loc2.getBlock())) {
                 if (teamSystem.blue.hasPlayer(player)) {
                     player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.RED + "You can not damage your own boat"));
-                } else if (loc2.getBlock().getType() == Material.OBSIDIAN) {
+                } else if (loc2.getBlock().getType() == Material.IRON_BLOCK) {
                     blueobbymap.put(String.valueOf(player), ib + 1);
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.BLUE + "You've hit obsidian " + ib + "/5"));
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.BLUE + "You've hit Iron Block " + ib + "/5"));
                     manager.addCurrencyToPlayer(player, 20);
                     player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 20$")));
 
-                } else if (loc2.getBlock().getType() == Material.OAK_PLANKS){
+                } else if (loc2.getBlock().getType() == Material.OAK_LOG){
                     bluewoodmap.put(String.valueOf(player), ibw +1);
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.BLUE + "You've hit wood " + ibw + "/2"));
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.BLUE + "You've hit Oak Log " + ibw + "/2"));
+                    manager.addCurrencyToPlayer(player, 10);
+                    player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 10$")));
                 }
                 if (ib == 5) {
-                    if(loc2.getBlock().getType() == Material.OBSIDIAN) {
+                    if(loc2.getBlock().getType() == Material.IRON_BLOCK) {
                         loc2.getBlock().setType(Material.AIR);
                         blueblock.remove(loc2);
                         manager.addCurrencyToPlayer(player, 50);
@@ -152,10 +162,12 @@ public class blockPlace implements Listener {
                     }
                 }
                 if (ibw == 2) {
-                    if(loc2.getBlock().getType() == Material.OAK_PLANKS) {
+                    if(loc2.getBlock().getType() == Material.OAK_LOG) {
                         loc2.getBlock().setType(Material.AIR);
                         redblock.remove(loc2);
                         bluewoodmap.replace(String.valueOf(player), 1);
+                        manager.addCurrencyToPlayer(player, 20);
+                        player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 20$")));
                     }
                 }
             }
@@ -164,26 +176,34 @@ public class blockPlace implements Listener {
             if (greenblock.contains(loc2.getBlock())) {
                 if (teamSystem.green.hasPlayer(player)) {
                     player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.RED + "You can not damage your own boat"));
-                } else if (loc2.getBlock().getType() == Material.OBSIDIAN) {
+                } else if (loc2.getBlock().getType() == Material.IRON_BLOCK) {
                     greenobbymap.put(String.valueOf(player), ig + 1);
                     player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.GREEN + "You've hit obsidian " + ig + "/5"));
+                    manager.addCurrencyToPlayer(player, 20);
+                    player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 20$")));
 
-                } else if (loc2.getBlock().getType() == Material.OAK_PLANKS){
+                } else if (loc2.getBlock().getType() == Material.OAK_LOG){
                     greenwoodmap.put(String.valueOf(player), igw +1);
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.GREEN + "You've hit wood " + igw + "/2"));
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.GREEN + "You've hit Oak Log " + igw + "/2"));
+                    manager.addCurrencyToPlayer(player, 10);
+                    player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 10$")));
                 }
                 if (ig == 5) {
-                    if(loc2.getBlock().getType() == Material.OBSIDIAN) {
+                    if(loc2.getBlock().getType() == Material.IRON_BLOCK) {
                         loc2.getBlock().setType(Material.AIR);
                         greenblock.remove(loc2);
                         greenobbymap.replace(String.valueOf(player), 1);
+                        manager.addCurrencyToPlayer(player, 50);
+                        player.sendMessage(Utils.chat((ChatColor.GOLD + "You received 50$")));
                     }
                 }
                 if (igw == 2) {
-                    if(loc2.getBlock().getType() == Material.OAK_PLANKS) {
+                    if(loc2.getBlock().getType() == Material.OAK_LOG) {
                         loc2.getBlock().setType(Material.AIR);
                         redblock.remove(loc2);
                         greenwoodmap.replace(String.valueOf(player), 1);
+                        manager.addCurrencyToPlayer(player, 20);
+                        player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 20$")));
                     }
                 }
             }
@@ -191,26 +211,35 @@ public class blockPlace implements Listener {
             if (yellowblock.contains(loc2.getBlock())) {
                 if (teamSystem.yellow.hasPlayer(player)) {
                     player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.RED + "You can not damage your own boat"));
-                } else if (loc2.getBlock().getType() == Material.OBSIDIAN) {
+                } else if (loc2.getBlock().getType() == Material.IRON_BLOCK) {
                     yellowobbymap.put(String.valueOf(player), iy + 1);
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.YELLOW + "You've hit obsidian " + iy + "/5"));
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.YELLOW + "You've hit Iron Block " + iy + "/5"));
+                    manager.addCurrencyToPlayer(player, 20);
+                    player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 20$")));
 
-                } else if (loc2.getBlock().getType() == Material.OAK_PLANKS){
+                } else if (loc2.getBlock().getType() == Material.OAK_LOG){
                     yellowwoodmap.put(String.valueOf(player), iyw +1);
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.YELLOW + "You've hit wood " + iyw + "/2"));
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.YELLOW + "You've hit Oak Log " + iyw + "/2"));
+                    manager.addCurrencyToPlayer(player, 10);
+                    player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 10$")));
+
                 }
                 if (iy == 5) {
-                    if(loc2.getBlock().getType() == Material.OBSIDIAN) {
+                    if(loc2.getBlock().getType() == Material.IRON_BLOCK) {
                         loc2.getBlock().setType(Material.AIR);
                         yellowblock.remove(loc2);
                         yellowobbymap.replace(String.valueOf(player), 1);
+                        manager.addCurrencyToPlayer(player, 50);
+                        player.sendMessage(Utils.chat((ChatColor.GOLD + "You received 50$")));
                     }
                 }
                 if (iyw == 2) {
-                    if(loc2.getBlock().getType() == Material.OAK_PLANKS) {
+                    if(loc2.getBlock().getType() == Material.OAK_LOG) {
                         loc2.getBlock().setType(Material.AIR);
                         yellowblock.remove(loc2);
                         yellowwoodmap.replace(String.valueOf(player), 1);
+                        manager.addCurrencyToPlayer(player, 20);
+                        player.sendMessage(Utils.chat((ChatColor.GOLD + "Hit! You received 20$")));
                     }
                 }
             }
